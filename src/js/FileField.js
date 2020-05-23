159e13983d24e203965dbafd2fc4da061b39cc5c
ilshaiah <m.ilshaiah@gmail.com>
2020-05-23 13:51:47 +0300

    change dependencies

--- src/js/FileField.js
+++ src/js/FileField.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import jQuery from 'jquery';
+
 
 class FileField extends React.Component{
 	constructor(props){
--- src/js/MultipleFileField.js
+++ src/js/MultipleFileField.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import jQuery from 'jquery';
+
 
 class MultipleFileField extends React.Component{
 	constructor(props){
