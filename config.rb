5897c756eb6f07e8ec3b9bb72862ee30c38751cb
= <m.ilshaiah@gmail.com>
2020-06-06 11:32:37 +0300

    edited .gitignore

---
+++ config.rb
@@ -0,0 +1,21 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "dist"
+sass_dir = "src"
+images_dir = "dist/images"
+javascripts_dir = "dist/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# [:nested, :expanded, :compact, or :compressed]
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+preferred_syntax = :scss
